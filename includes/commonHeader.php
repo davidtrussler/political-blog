548aa906326ff1e3f15a773ea521137b70da9b89
David Trussler <mail@davidtrussler.net>
2015-09-01 08:01:55 +0100

    Removed references to  CSS and JS - commented out to allow for re-use with changed/new files

--- includes/commonHeader.php
+++ includes/commonHeader.php
@@ -27,9 +27,11 @@ $link_array = array (
 		<title>david trussler | <?php echo $title; ?></title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<script type="text/javascript" src="<?php echo $SERVER_ROOT; ?>js/jquery-1.7.1.min.js"></script>
+		<!-- 
+		<script type="text/javascript" src="<?php echo $SERVER_ROOT; ?>js/jquery-1.7.1.min.js"></script> 
 		<script type="text/javascript" src="<?php echo $SERVER_ROOT; ?>js/dtPage.js"></script>
 		<link rel="stylesheet" type="text/css" href="<?php echo $SERVER_ROOT; ?>css/dtPage.css"/>
+		-->
 		<link rel="icon" type="image/jpg" href="<?php echo $root; ?>graphics/favicon.png">
 
 <?php
